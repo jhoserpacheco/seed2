b9699945bcb51d84c8df972100f05be9d3a9e1a9
jhoserpacheco <jhoserpacheco@gmail.com>
2021-10-23 12:52:15 -0500

    Modificaciones en el modelo, se agregaron nuevas clases en model.py

--- seed/admin.py
+++ seed/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Docente, Grupo, Estudiante
+from .models import Docente, Grupo, Estudiante, Actividad
 
 admin.site.register(Docente)
 admin.site.register(Grupo)
 admin.site.register(Estudiante)
+admin.site.register(Actividad)
--- seed/models.py
+++ seed/models.py
@@ -16,7 +16,7 @@ class Docente(models.Model):
 class Grupo(models.Model):
     codigo_grupo = models.CharField(max_length=50, primary_key=True)
     docente = models.ForeignKey(Docente, on_delete = models.CASCADE)
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50,default="")
     def __str__(self):
         return self.codigo_grupo
 
@@ -26,3 +26,27 @@ class Estudiante(models.Model):
     nombre_estudiante = models.CharField(max_length=50, default="")
     def __str__(self):
         return self.email
+
+class Tema(models.Model): 
+    codigo_tema = models.IntegerField(primary_key=True)
+    nombre_tema = models.CharField(max_length=50, default="")
+    grupo_tema = models.OneToOneField(Grupo, on_delete = models.CASCADE)
+
+class Actividad(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    nombre_ac = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=350)
+    estudiante_actividad = models.ManyToManyField(Estudiante)
+    tema_actividad = models.ForeignKey(Tema, on_delete = models.CASCADE, default="")
+    def __str__(self):
+        return self.nombre_ac
+
+class EstructuraDeDatos(models.Model):
+    codigo_ed = models.IntegerField(primary_key=True)
+    nombre_ed = models.CharField(max_length=50)
+    actividad = models.OneToOneField(Actividad, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.nombre_ed
+
+
+
