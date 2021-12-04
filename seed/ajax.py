b1f2b694aafe98102751549ac1929ed5a22cb2c3
Ronald Benitez <ronaldeduardobm@ufps.edu.co>
2021-12-03 23:57:52 -0500

    se agrego boton eliminar a temas en todos los templates (funciona)

--- seed/ajax.py
+++ seed/ajax.py
@@ -1,10 +1,17 @@
 from django.http import JsonResponse
-from .models import Grupo
+from .models import *
 
-def eliminar_identificador(request):
-    print("eliminar_identificador............",request)
+def eliminar_grupo(request):
     pk = request.GET.get('identificador_id')
     identificador = Grupo.objects.get(pk=pk)
+    identificador.delete()
+    response = {}
+    return JsonResponse(response)
+
+def eliminar_tema(request):
+    print("eliminar_tema...........", request)
+    pk = request.GET.get('identificador_id')
+    identificador = Tema.objects.get(pk=pk)
     identificador.delete()
     response = {}
     return JsonResponse(response)
